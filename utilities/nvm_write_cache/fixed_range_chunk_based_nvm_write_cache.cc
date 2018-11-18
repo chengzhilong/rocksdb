9d2b1fbc09a10509460fda46100c0268e72d855d
Yiwen <zzyyyww9595@gmail.com>
2018-11-18 21:21:13 +0800

    2018-11-15 fixed range chunk update

--- utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
+++ utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.cc
@@ -1,52 +1,102 @@
-//
-// Created by zzyyy on 2018/11/2.
-//
+#include "skiplist/libpmemobj++/make_persistent.hpp"
+#include "skiplist/libpmemobj++/make_persistent_array.hpp"
+#include "skiplist/libpmemobj++/p.hpp"
+#include "skiplist/libpmemobj++/persistent_ptr.hpp"
+#include "skiplist/libpmemobj++/pool.hpp"
+#include "skiplist/libpmemobj++/transaction.hpp"
 
-#include "utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.h"
+#include "fixed_range_chunk_based_nvm_write_cache.h"
+
+#include <ex_common.h>
+
+
 namespace rocksdb {
-    FixedRangeChunkBasedNVMWriteCache::FixedRangeChunkBasedNVMWriteCache(
-            rocksdb::FixedRangeBasedOptions *cache_options_)
-            :
-                internal_options_(cache_options_),
-                cache_stats_(new FixedRangeChunkBasedCacheStats),
-                range_seq_(0)
-                {
-        cache_stats_->range_list_.clear();
-        cache_stats_->chunk_bloom_data_.clear();
-        cache_stats_->used_bits_ = 0;
 
+    using p_range::pmem_hash_map;
+
+    FixedRangeChunkBasedNVMWriteCache::FixedRangeChunkBasedNVMWriteCache(const string& file, const string& layout)
+            :file_path(file)
+            ,LAYOUT(layout)
+            ,POOLSIZE(1 << 26)
+    {
+        //    file_path.data()
+        bool justCreated = false;
+        if (file_exists(file_path) != 0) {
+            pop = pool<pmem_hash_map>::create(file_path, LAYOUT, POOLSIZE, CREATE_MODE_RW);
+            justCreate = true;
+
+        } else {
+            pop = pool<pmem_hash_map>::open(file_path, LAYOUT);
+        }
+//  pmem_ptr = pop.root();
+
+        transaction::run(pop, [&] {
+            persistent_ptr<p_range::pmem_hash_map> p_map = pop.root();
+            if (justCreated) {
+                // TODO 配置
+                p_map->tabLen = ;
+                p_map->tab = make_persistent<p_node[]>(p_map->tabLen);
+                p_map->loadFactor = 0.75f;
+                p_map->threshold = p_map->tabLen * p_map->loadFactor;
+                p_map->size = 0;
+            }
+        });
+
+//  if (file_exists(file_path) != 0) {
+//    if ((pop = pmemobj_create(file_path, POBJ_LAYOUT_NAME(range_mem),
+//                              PMEMOBJ_MIN_POOL, 0666)) == NULL) {
+//      perror("failed to create pool\n");
+//      return 1;
+//    }
+//  } else {
+//    if ((pop = pmemobj_open(file_path,
+//                            POBJ_LAYOUT_NAME(range_mem))) == NULL) {
+//      perror("failed to open pool\n");
+//      return 1;
+//    }
+//  }
     }
 
-    FixedRangeChunkBasedNVMWriteCache::~FixedRangeChunkBasedNVMWriteCache() {
-        delete cache_stats_;
+    FixedRangeChunkBasedNVMWriteCache::~FixedRangeChunkBasedNVMWriteCache()
+    {
+        if (pop)
+            pop.close();
+//    pmemobj_close(pop);
     }
 
-    Status FixedRangeChunkBasedNVMWriteCache::Get(const rocksdb::Slice &key, std::string *value) {
-        return Status::OK();
+    Status FixedRangeChunkBasedNVMWriteCache::Get(const Slice &key, std::string *value)
+    {
+        FixedRangeTab *tab;
+        PrefixExtractor* prefix_extractor = internal_options_->prefix_extractor_;
+        tab = cache_stats_->range_list_[prefix_extractor(std::string(key.data(), key.size()))];
+        if(tab != nullptr){
+            // 找到了对应的tab
+            return tab->Get(key, value);
+        }else{
+            // 没有对应的tab，返回未找到
+            return Status::kNotFound;
+        }
+        // TODO
+        // 1. calc target FixedRangeTab
+        // 2.
     }
 
-    Status FixedRangeChunkBasedNVMWriteCache::Insert(const rocksdb::Slice &cached_data, void *insert_mark) {
-        printf("insert data\n");
-        cache_stats_->used_bits_ += cached_data.size_;
-        return Status::OK();
+    void FixedRangeChunkBasedNVMWriteCache::addCompactionRangeTab(FixedRangeTab *tab)
+    {
+        range_queue_.pu
     }
 
-    uint64_t FixedRangeChunkBasedNVMWriteCache::NewRange(const std::string &prefix) {
-        printf("new range %s\n", prefix.c_str());
-        return ++range_seq_;
+    uint64_t FixedRangeChunkBasedNVMWriteCache::NewRange(const std::string &prefix)
+    {
+        // ( const void * key, int len, unsigned int seed );
+//  uint64_ _hash = CityHash64WithSeed(prefix, prefix.size(), 16);
+        persistent_ptr<p_range::pmem_hash_map> p_map = pop.root();
+        size_t bufSize = 1 << 26; // 64 MB
+        uint64_t _hash;
+        _hash = p_map->put(pop, prefix, bufSize);
+        return _hash;
     }
 
-    Iterator* FixedRangeChunkBasedNVMWriteCache::NewIterator() {
-        return nullptr;
-    }
-
-    CompactionItem* FixedRangeChunkBasedNVMWriteCache::GetCompactionData() {
-        return nullptr;
-    }
+} // namespace rocksdb
 
 
-
-
-
-
-}// end rocnsdb
--- utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.h
+++ utilities/nvm_write_cache/fixed_range_chunk_based_nvm_write_cache.h
@@ -1,58 +1,66 @@
-//
-// Created by zzyyy on 2018/11/2.
-// 胡泽鑫负责
-//
+#ifndef FIXED_RANGE_CHUNK_BASED_NVM_WRITE_CACHE_H
+#define FIXED_RANGE_CHUNK_BASED_NVM_WRITE_CACHE_H
 
-#pragma once
 
 #include <queue>
+#include <rocksdb/iterator.h>
 #include "utilities/nvm_write_cache/nvm_write_cache.h"
 #include "utilities/nvm_write_cache/nvm_cache_options.h"
 
+#include "skiplist/libpmemobj++/pool.hpp"
 
-namespace rocksdb{
+#include <fixed_range_tab.h>
+#include <pmem_hash_map.h>
 
-    class FixedRange;
+namespace rocksdb {
+
+    using std::string;
+    using namespace pmem;
+    using namespace pmem::obj;
 
     struct FixedRangeChunkBasedCacheStats{
-
         uint64_t  used_bits_;
-
-        std::unordered_map<std::string, uint64_t> range_list_;
-
+        std::unordered_map<std::string, FixedRangeTab* > range_list_;
         std::vector<std::string*> chunk_bloom_data_;
-
     };
 
-    struct CompactionItem{
-        FixedRange* pending_compated_range_;
+    struct CompactionItem {
+        FixedRangeTab* pending_compated_range_;
         Slice start_key_, end_key_;
-        uint64_t range_size_, chunk_num_;
+        uint64_t range_size_, range_rum_;
     };
 
-    class FixedRangeChunkBasedNVMWriteCache: public NVMWriteCache{
-
+    class FixedRangeChunkBasedNVMWriteCache : public NVMWriteCache
+    {
     public:
-
-        explicit FixedRangeChunkBasedNVMWriteCache(FixedRangeBasedOptions* cache_options_);
-
-        ~FixedRangeChunkBasedNVMWriteCache() override;
+        FixedRangeChunkBasedNVMWriteCache(const string& file, const string& layout);
+        ~FixedRangeChunkBasedNVMWriteCache();
 
         // insert data to cache
         // insert_mark is (uint64_t)range_id
-        Status Insert(const Slice& cached_data, void* insert_mark) override;
+//  Status Insert(const Slice& cached_data, void* insert_mark) override;
 
         // get data from cache
         Status Get(const Slice& key, std::string* value) override;
 
+
         // get iterator of the total cache
         Iterator* NewIterator() override;
+
+        // 获取range_mem_id对应的RangeMemtable结构
+//  FixedRangeTab* GetRangeMemtable(uint64_t range_mem_id);
 
         // return there is need for compaction or not
         bool NeedCompaction() override {return !range_queue_.empty();}
 
         //get iterator of data that will be drained
-        CompactionItem* GetCompactionData();
+        // get 之后释放没有 ?
+        CompactionItem* GetCompactionData() {
+            CompactionItem *item = range_queue_.front();
+//    range_queue_.pop();
+            return item;
+        }
+        void addCompactionRangeTab(FixedRangeTab *tab);
 
         // add a range with a new prefix to range mem
         // return the id of the range
@@ -63,13 +71,23 @@ namespace rocksdb{
 
         // get stats of this cache
         FixedRangeChunkBasedCacheStats* stats(){return cache_stats_;}
+    private:
+        string file_path;
+        const string LAYOUT;
+        const size_t POOLSIZE;
+
+        //  persistent_queue<FixedRangeTab> range_mem_list_;
+//  persistent_map<range, FixedRangeTab> range2tab;
+        //  persistent_queue<uint64_t> compact_queue_;
+
+        pool<p_range::pmem_hash_map> pop;
 
     private:
         const FixedRangeBasedOptions* internal_options_;
         FixedRangeChunkBasedCacheStats* cache_stats_;
         std::queue<CompactionItem*> range_queue_;
         uint64_t range_seq_;
-
-
     };
-}
+
+} // namespace rocksdb
+#endif // FIXED_RANGE_CHUNK_BASED_NVM_WRITE_CACHE_H
