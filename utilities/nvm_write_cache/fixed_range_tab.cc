a5f0283030b8bf2f48210bc616cd9c65d902e2c2
Yiwen <zzyyyww9595@gmail.com>
2018-11-28 20:57:07 +0800

    2018-11-28 bug fix #9.4 debug

--- utilities/nvm_write_cache/fixed_range_tab.cc
+++ utilities/nvm_write_cache/fixed_range_tab.cc
@@ -193,7 +193,8 @@ void FixedRangeTab::CheckAndUpdateKeyRange(const InternalKeyComparator &icmp, co
     GetRealRange(cur_start, cur_end);
     cout<<"cur_start["<<cur_start.data()<<"]"<<"cur_end["<<cur_end.data()<<"]"<<endl;
     cout<<"new_start["<<new_start.data()<<"]["<<new_start.size()<<"], new_end["<<new_end.data()<<"]["<<new_end.size()<<"]"<<endl;
-    cout<<icmp.Compare(cur_start new_start)<<endl;
+
+    cout<<icmp.Compare(cur_start, new_start)<<endl;
     if (cur_start.size() == 0 || icmp.Compare(cur_start, new_start) >= 0) {
         cout<<"update start"<<endl;
         cur_start = new_start;
